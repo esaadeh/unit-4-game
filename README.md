9d103852c0eabd69240d828a7c01d7d4876231f6
EKS <saadeh.emad@gmail.com>
2018-11-14 16:53:56 -0800

    Final Submission

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # unit-4-game
+
+
+Instructions:
+
+You will be given a random number at the start of the game.
+
+There are four crystals below. By Clicking on a crystal you will add a specific amount of points to your total score.
+
+You win the game by matching you total score to random number. You lose the the game if you total score goes above the random number.
+
+The value of each crystal is hidden from you until you click on it
+
+Each time when the game starts the game will change he values of each crystal.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,102 +6,61 @@ var total = 0;
 
 // Randomly generated values for the gems (1-12)
 var gem1 = (Math.floor(Math.random() * 12) + 1);
-console.log("gem 1: ", gem1);
 var gem2 = (Math.floor(Math.random() * 12) + 1);
-console.log("gem 2: ", gem2);
 var gem3 = (Math.floor(Math.random() * 12) + 1);
-console.log("gem 3: ", gem3);
 var gem4 = (Math.floor(Math.random() * 12) + 1);
-console.log("gem 4: ", gem4);
 
-// Randomly generated value for the num to be matched (19-120) and display on browser
+// Randomly generated value for the num to be matched (19-120)
 var match = (Math.floor(Math.random() * 101) + 19);
-console.log(match);
 
+// Display initial goal and the total scor of 0.
 $("#goal").html("Goal: " + match);
+$("#score").html(total);
+
+
+// Reset function to re-initialize the gem values and the goal value after a loss or win 
+function reset() {
+    gem1 = (Math.floor(Math.random() * 12) + 1);
+    gem2 = (Math.floor(Math.random() * 12) + 1);
+    gem3 = (Math.floor(Math.random() * 12) + 1);
+    gem4 = (Math.floor(Math.random() * 12) + 1);
+    match = (Math.floor(Math.random() * 101) + 19);
+    total = 0;
+    $("#goal").html("Goal: " + match);
+    $("#wins-total").html("Wins: " + wins);
+    $("#losses-total").html("Losses: " + losses);
+    $("#score").html(total);
+}
 
 
 
-
-
-
+// Game logic...click on first gem, increase the total score by the amount associated with gem1
 $("#gem1").on("click", function () {
     total = total + gem1;
+    // Display the running score after every click on the gem   
     $("#score").html(total);
-    console.log("total: ", total);
-
+    // Check if the total matched the goal for a win and if so, increment wins and run the reset function 
     if (total === match) {
         wins++
-        total = 0;
-        console.log("win");
-        console.log("Win total: ", wins);
-        $("#wins-total").html("Wins: " + wins);
-        $("#losses-total").html("Losses: " + losses);
-        $("#score").html(total);
-        gem1 = (Math.floor(Math.random() * 12) + 1);
-        console.log("gem 1: ", gem1);
-        gem2 = (Math.floor(Math.random() * 12) + 1);
-        console.log("gem 2: ", gem2);
-        gem3 = (Math.floor(Math.random() * 12) + 1);
-        console.log("gem 3: ", gem3);
-        gem4 = (Math.floor(Math.random() * 12) + 1);
-        console.log("gem 4: ", gem4);
-        match = (Math.floor(Math.random() * 101) + 19);
-        $("#goal").html("Goal: " + match);
-
-      
+        reset();
+// Otherwise, check if player went over the goal and if so increment losses and run reset function
     } else if (total > match) {
         losses++;
-        total = 0;
-        console.log("lose");
-        console.log("lose total: ", losses);
-        $("#wins-total").html("Wins: " + wins);
-        $("#losses-total").html("Losses: " + losses);
-        $("#score").html(total);
-        gem1 = (Math.floor(Math.random() * 12) + 1);
-        console.log("gem 1: ", gem1);
-        gem2 = (Math.floor(Math.random() * 12) + 1);
-        console.log("gem 2: ", gem2);
-        gem3 = (Math.floor(Math.random() * 12) + 1);
-        console.log("gem 3: ", gem3);
-        gem4 = (Math.floor(Math.random() * 12) + 1);
-        console.log("gem 4: ", gem4);
-        match = (Math.floor(Math.random() * 101) + 19);
-        $("#goal").html("Goal: " + match);
+        reset();
     }
 });
 
-
+// Same logic as above for the other 4 gems
 
 $("#gem2").on("click", function () {
     total = total + gem2;
     $("#score").html(total);
-    console.log("total: ", total);
-
     if (total === match) {
         wins++
-        total = 0;
-        $("#wins-total").html("Wins: " + wins);
-        $("#losses-total").html("Losses: " + losses);
-        $("#score").html(total);
-        gem1 = (Math.floor(Math.random() * 12) + 1);
-        gem2 = (Math.floor(Math.random() * 12) + 1);
-        gem3 = (Math.floor(Math.random() * 12) + 1);
-        gem4 = (Math.floor(Math.random() * 12) + 1);
-        match = (Math.floor(Math.random() * 101) + 19);
-        $("#goal").html("Goal: " + match);
+        reset();
     } else if (total > match) {
         losses++;
-        total = 0;
-        $("#wins-total").html("Wins: " + wins);
-        $("#losses-total").html("Losses: " + losses);
-        $("#score").html(total);
-        gem1 = (Math.floor(Math.random() * 12) + 1);
-        gem2 = (Math.floor(Math.random() * 12) + 1);
-        gem3 = (Math.floor(Math.random() * 12) + 1);
-        gem4 = (Math.floor(Math.random() * 12) + 1);
-        match = (Math.floor(Math.random() * 101) + 19);
-        $("#goal").html("Goal: " + match);
+        reset();
     }
 });
 
@@ -109,31 +68,12 @@ $("#gem2").on("click", function () {
 $("#gem3").on("click", function () {
     total = total + gem3;
     $("#score").html(total);
-    console.log("total: ", total);
     if (total === match) {
         wins++
-        total = 0;
-        $("#wins-total").html("Wins: " + wins);
-        $("#losses-total").html("Losses: " + losses);
-        $("#score").html(total);
-        gem1 = (Math.floor(Math.random() * 12) + 1);
-        gem2 = (Math.floor(Math.random() * 12) + 1);
-        gem3 = (Math.floor(Math.random() * 12) + 1);
-        gem4 = (Math.floor(Math.random() * 12) + 1);
-        match = (Math.floor(Math.random() * 101) + 19);
-        $("#goal").html("Goal: " + match);
+        reset();
     } else if (total > match) {
         losses++;
-        total = 0;
-        $("#wins-total").html("Wins: " + wins);
-        $("#losses-total").html("Losses: " + losses);
-        $("#score").html(total);
-        gem1 = (Math.floor(Math.random() * 12) + 1);
-        gem2 = (Math.floor(Math.random() * 12) + 1);
-        gem3 = (Math.floor(Math.random() * 12) + 1);
-        gem4 = (Math.floor(Math.random() * 12) + 1);
-        match = (Math.floor(Math.random() * 101) + 19);
-        $("#goal").html("Goal: " + match);
+        reset();
     }
 });
 
@@ -141,37 +81,14 @@ $("#gem3").on("click", function () {
 $("#gem4").on("click", function () {
     total = total + gem4;
     $("#score").html(total);
-    console.log("total: ", total);
     if (total === match) {
         wins++
-        total = 0;
-        $("#wins-total").html("Wins: " + wins);
-        $("#losses-total").html("Losses: " + losses);
-        $("#score").html(total);
-        gem1 = (Math.floor(Math.random() * 12) + 1);
-        gem2 = (Math.floor(Math.random() * 12) + 1);
-        gem3 = (Math.floor(Math.random() * 12) + 1);
-        gem4 = (Math.floor(Math.random() * 12) + 1);
-        match = (Math.floor(Math.random() * 101) + 19);
-        $("#goal").html("Goal: " + match);
+        reset();
     } else if (total > match) {
         losses++;
-        total = 0;
-        $("#wins-total").html("Wins: " + wins);
-        $("#losses-total").html("Losses: " + losses);
-        $("#score").html(total);
-        gem1 = (Math.floor(Math.random() * 12) + 1);
-        gem2 = (Math.floor(Math.random() * 12) + 1);
-        gem3 = (Math.floor(Math.random() * 12) + 1);
-        gem4 = (Math.floor(Math.random() * 12) + 1);
-        match = (Math.floor(Math.random() * 101) + 19);
-        $("#goal").html("Goal: " + match);
+        reset();
     }
 });
 
-
-// Display the game progression
-$("#wins-total").html("Wins: " + wins);
-$("#losses-total").html("Losses: " + losses);
 
 
